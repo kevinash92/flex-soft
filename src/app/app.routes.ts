4dd1d236dc81574d85fd915a69ff36ab5c3f8e7c
mebara <mebara@ingenieurpc>
2018-08-06 00:58:38 +0100

    first edit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -20,7 +20,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>. kevi
  */
 
 import { Routes } from '@angular/router';
